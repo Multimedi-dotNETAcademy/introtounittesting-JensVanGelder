11b645c81046393b9a0bca5a0b6c06d40c752654
JensVanGelder <jens.v.gelder@gmail.com>
2021-01-25 14:17:04 +0100

    Finished DPCalc,DateHelper

---
+++ TestNinja.UnitTests/DateHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    internal class DateHelperTests
+    {
+        [TestCase("22/02/2021","1/03/2021")]
+        [TestCase("22/12/2021", "1/01/2022")]
+        public void FirstOfNextMonth_WhenCalled_ReturnDateTimeFirstNextMonth(string date, string resultDate)
+        {
+            DateTime testDate = DateTime.Parse(date);
+            DateTime expectedResult = DateTime.Parse(resultDate);
+            DateTime result = DateHelper.FirstOfNextMonth(testDate);
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
